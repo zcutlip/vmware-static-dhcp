1335f1c5f7dbc607bb067cd4ab9bdb784eeb894c
Zachary Cutlip <uid000@gmail.com>
2019-11-08 14:33:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# "vmware_static_dhcp"
+
+## Description
+
+"VMware Fusion static DHCP/hosts file setup tool"
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+about = {}
+with open("vmware_static_dhcp/__about__.py") as fp:
+    exec(fp.read(), about)
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__summary__"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Zachary Cutlip",
+      author_email="",
+      url="MIT",
+      packages=['vmware_static_dhcp'],
+      entry_points={
+          'console_scripts': ['vmwarestatic=vmware_static_dhcp.cli:main'], },
+      python_requires='>=3.7',
+      install_requires=[],
+      package_data={'vmware_static_dhcp': ['config/*']},
+      )
