07d8b6234e9c61e67cce230162f94d8e8df1540e
Zachary Cutlip <uid000@gmail.com>
2019-11-11 18:00:16 -0800

    Export more things from __init__.__all__

--- vmware_static_dhcp/__init__.py
+++ vmware_static_dhcp/__init__.py
@@ -1,11 +1,16 @@
 from .__about__ import (
     __version__,
     __title__,
+    __cli_title__,
     __summary__
 )
+
+from .vmware_static_dhcp import VMWareStaticDhcp
 
 __all__ = [
     "__version__",
     "__title__",
-    "__summary__"
+    "__cli_title__",
+    "__summary__",
+    "VMWareStaticDhcp"
 ]
