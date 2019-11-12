4bf34a2ac40110cc81d2db60615b686c1153f2bf
Zachary Cutlip <uid000@gmail.com>
2019-11-11 18:00:28 -0800

    refactor cli.py

--- vmware_static_dhcp/cli.py
+++ vmware_static_dhcp/cli.py
@@ -1,21 +1,6 @@
 import sys
-
-from .version import Vmware_static_dhcpAbout as About
-
-
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-
-    print("vmware_static_dhcp main()")
-    print("%s" % About())
-    if argv:
-        print("args:")
-        for arg in argv:
-            print(arg)
-    else:
-        print("No args provided")
+from .vmware_static_dhcp import main as vmware_main
 
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+def main():
+    vmware_main(sys.argv[1:])
